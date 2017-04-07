29319a4e57af1c27ca6d6a74c49b695f40c821ca
Tony Milici <tonymilici@tonys-mbp>
2017-04-06 23:05:26 -0700

    boulders

--- BoulderingGuides/ClusterViewController.swift
+++ BoulderingGuides/ClusterViewController.swift
@@ -10,13 +10,35 @@ import Foundation
 import UIKit
 
 public class ClusterViewController: UITableViewController {
+    var _boulders: [Boulder]?
+    let cellId = "BoulderCell"
+    
     public init(cluster: Cluster) {
         super.init(nibName: "ClusterView", bundle: nil)
         title = cluster.name
+        _boulders = cluster.boulders
     }
     
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    public override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (_boulders?.count)!
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: cellId) {
+            cell.textLabel?.text = _boulders?[indexPath.row].name
+            return cell
+        }
+        let cell = UITableViewCell()
+        cell.textLabel?.text = _boulders?[indexPath.row].name
+        cell.accessoryType = UITableViewCellAccessoryType.disclosureIndicator
+        return cell
+    }
 }

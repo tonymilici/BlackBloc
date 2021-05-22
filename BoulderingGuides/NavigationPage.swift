fa398ed8ef940ad107d7644990f5587704113358
Tony Milici <amilici@guess.com>
2021-05-22 08:40:32 -0700

    Navigate to Boulder

--- BoulderingGuides/NavigationPage.swift
+++ BoulderingGuides/NavigationPage.swift
@@ -3,8 +3,15 @@
 //  BoulderingGuides
 //
 //  Created by Tony Milici on 5/22/21.
-//  Copyright © 2021 Imortal Apps. All rights reserved.
+//  Copyright © 2021 BlackBloc Software. All rights reserved.
 //
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE./
 
 import SwiftUI
 
@@ -12,7 +19,9 @@ struct NavigationPage: View {
     let boulder: Boulder
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationMapView(location: boulder.location!)
+            .navigationTitle(boulder.name)
+            .navigationBarTitleDisplayMode(.inline)
     }
 }
 

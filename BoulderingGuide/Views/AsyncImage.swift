c32220f3b022d4f87b838a37a202975256d5d85d
Tony Milici <amilici@guess.com>
2021-05-30 14:49:07 -0700

    AsyncImage

---
+++ BoulderingGuide/Views/AsyncImage.swift
@@ -0,0 +1,56 @@
+//
+//  AsyncImage.swift
+//  BoulderingGuide
+//
+//  Created by Tony Milici on 5/30/21.
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE./
+
+import SwiftUI
+
+struct AsyncImage: View {
+    @StateObject var provider:  ImageProvider
+    
+    init(url: String) {
+        _provider = StateObject(wrappedValue: ImageProvider(url: url))
+    }
+    
+    var body: some View {
+        content
+            .onAppear(perform: provider.load)
+    }
+
+    private var content: some View {
+        Group {
+            if provider.image != nil {
+                Image(uiImage: provider.image!)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } else {
+                Text("Loading...")
+            }
+        }
+    }
+}
+
+struct AsyncImage_Previews: PreviewProvider {
+    static var previews: some View {
+        AsyncImage(url: "https://res.cloudinary.com/blackbloc-software/image/upload/v1622386927/aftershock_tkt8py.png")
+    }
+}
